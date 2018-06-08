8228fbc2c929fa980b168c006845d4a15c3da949
Nataliia Pakhol <nataliia.pakhol@ring.com>
2018-06-08 17:06:45 +0300

    Added *.tf for to create vpc with all the attributes and bastion host

---
+++ terraform_project/bastion_host/infrastructure.tf
@@ -0,0 +1,183 @@
+#Access credentials
+
+provider "aws" {
+  access_key = ""
+  secret_key = ""
+  region     = "${var.aws_region}"
+}
+
+#Create aws vpc
+
+resource "aws_vpc" "vpc" {
+  cidr_block           = "${var.vpc_cidr}"
+  enable_dns_hostnames = true
+  enable_dns_support   = true
+
+  tags {
+    Name = "VPC_np"
+  }
+}
+
+#Internet gateway for the public subnet
+
+resource "aws_internet_gateway" "ig" {
+  vpc_id = "${aws_vpc.vpc.id}"
+
+  tags {
+    Name = "igw_np"
+  }
+}
+
+#Elastic IP for NAT
+
+resource "aws_eip" "nat_eip" {
+  vpc = true
+}
+
+#NAT gateway 
+
+resource "aws_nat_gateway" "nat" {
+  allocation_id = "${aws_eip.nat_eip.id}"
+  subnet_id     = "${aws_subnet.public_subnet.id}"
+
+  tags {
+    Name = "nat_np"
+  }
+}
+
+#Define public subnet
+
+resource "aws_subnet" "public_subnet" {
+  vpc_id                  = "${aws_vpc.vpc.id}"
+  cidr_block              = "${var.public_subnet_cidr}"
+  availability_zone       = "${var.availability_zone}"
+  map_public_ip_on_launch = true
+
+  tags {
+    Name = "public-subnet_np"
+  }
+}
+
+#Define private subnet
+
+resource "aws_subnet" "private_subnet" {
+  vpc_id                  = "${aws_vpc.vpc.id}"
+  cidr_block              = "${var.private_subnet_cidr}"
+  map_public_ip_on_launch = false
+  availability_zone       = "${var.availability_zone}"
+
+  tags {
+    Name = "private-subnet_np"
+  }
+}
+
+#Define routing table for private subnet
+
+resource "aws_route_table" "private" {
+  vpc_id = "${aws_vpc.vpc.id}"
+
+  tags {
+    Name = "private-route-table_np"
+  }
+}
+
+# Define routing table for public subnet
+
+resource "aws_route_table" "public" {
+  vpc_id = "${aws_vpc.vpc.id}"
+
+  tags {
+    Name = "public-route-table_np"
+  }
+}
+
+resource "aws_route" "public_internet_gateway" {
+  route_table_id         = "${aws_route_table.public.id}"
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = "${aws_internet_gateway.ig.id}"
+}
+
+resource "aws_route" "private_nat_gateway" {
+  route_table_id         = "${aws_route_table.private.id}"
+  destination_cidr_block = "0.0.0.0/0"
+  nat_gateway_id         = "${aws_nat_gateway.nat.id}"
+}
+
+# Route table associations
+resource "aws_route_table_association" "public" {
+  subnet_id      = "${aws_subnet.public_subnet.id}"
+  route_table_id = "${aws_route_table.public.id}"
+}
+
+resource "aws_route_table_association" "private" {
+  subnet_id      = "${aws_subnet.private_subnet.id}"
+  route_table_id = "${aws_route_table.private.id}"
+}
+
+# Define security group for the bastion host instance
+
+resource "aws_security_group" "bastion" {
+  vpc_id      = "${aws_vpc.vpc.id}"
+  name        = "bastion-host"
+  description = "Allow SSH to bastion host"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["217.20.170.129/32"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags {
+    Name = "bastion-sg"
+  }
+}
+
+#Launch the bastion instance
+
+resource "aws_instance" "bastion" {
+  ami                    = "var.bastion_ami"
+  instance_type          = "t2.micro"
+  key_name               = "${var.key_name}"
+  monitoring             = true
+  vpc_security_group_ids = ["${aws_security_group.bastion.id}"]
+  subnet_id              = "${aws_subnet.public_subnet.id}"
+
+  tags {
+    Name = "bastion_np"
+  }
+}
+
+#Attaching a EIP to bastion host
+resource "aws_eip" "bastion_eip" {
+  instance = "${aws_instance.bastion.id}"
+}
+
+#Adding the OpsWorks stack
+
+resource "aws_opsworks_stack" "bastion host" {
+  name                         = "bastion_host_np"
+  region                       = "${var.aws_region}"
+  vpc_id                       = "${aws_vpc.vpc.id}"
+  service_role_arn             = "${aws_iam_role.opsworks.arn}"
+  default_instance_profile_arn = "${aws_iam_instance_profile.opsworks.arn}"
+
+  tags {
+    Name = "np_stack"
+  }
+
+  custom_json = <<EOT
+{
+ "foobar": {
+    "version": "1.0.0"
+  }
+}
+EOT
+}
